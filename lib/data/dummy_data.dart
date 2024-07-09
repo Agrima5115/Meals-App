f36bb66288a061b16dc38f0b7baea95713b04b27
Agrima5115 <agrimarajput095@gmail.com>
2024-07-09 13:13:54 +0530

    category screen added

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,54 @@
+import 'package:meals/models/category.dart';
+import 'package:flutter/material.dart';
+const availableCategories = [
+  Category(
+    id: 'c1',
+    title: 'Breakfast & Brunch',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Italian',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Mexican',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Indian',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Chinese',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Healthy & Low-Calorie',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Beverages',
+    color: Colors.lightBlue,
+  ),
+   Category(
+    id: 'c8',
+    title: 'Bakery & Breads',
+    color: Colors.lightGreen,
+  ),
+   Category(
+    id: 'c9',
+    title: 'Kid-Friendly Meals',
+    color: Colors.pink,
+  ),
+   Category(
+    id: 'c10',
+    title: 'Desserts & Sweets',
+    color: Colors.teal,
+  ),
+];
